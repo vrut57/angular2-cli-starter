5c737e0d46a975ee311e07c597a996e002e9a50c
vrut57 <vrut57@gmail.com>
2017-05-09 20:26:03 -0400

    angular 2/express project built with cli

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+
+import {PostsService} from './posts.service';
+
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full'
+  },{
+    path: 'posts',
+    component: PostsComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [PostsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
